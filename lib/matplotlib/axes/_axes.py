abc7af24d28fbf29b1d7c20b41ed46d800e9aa44
Ryan Morshead <ryan.morshead@gmail.com>
2015-07-23 11:39:11 -0700

    see TODO

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -5272,7 +5272,7 @@ class Axes(_AxesBase):
         y = Y.compressed()
 
         # Transform from native to data coordinates?
-        t = collection._transform
+        t = collection.transform
         if (not isinstance(t, mtransforms.Transform) and
             hasattr(t, '_as_mpl_transform')):
             t = t._as_mpl_transform(self.axes)
@@ -5421,7 +5421,7 @@ class Axes(_AxesBase):
         self.grid(False)
 
         # Transform from native to data coordinates?
-        t = collection._transform
+        t = collection.transform
         if (not isinstance(t, mtransforms.Transform) and
             hasattr(t, '_as_mpl_transform')):
             t = t._as_mpl_transform(self.axes)
