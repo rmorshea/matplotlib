763bfbed3051addd9bbfcb5614e8b0749e484a08
Ryan Morshead <ryan.morshead@gmail.com>
2015-08-04 18:10:25 -0700

    add figure traitlet

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -3891,7 +3891,6 @@ class Axes(_AxesBase):
                 transOffset=kwargs.pop('transform', self.transData),
                 alpha=alpha
                 )
-        #!TODO : not sure of how to handle collections
         collection.transform = mtransforms.IdentityTransform()
         collection.update(kwargs)
 
