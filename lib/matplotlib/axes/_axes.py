fb767cd2daf40c6ef145552106c70b6227883a7f
Ryan Morshead <ryan.morshead@gmail.com>
2015-08-25 14:35:44 -0700

    base artist refactor complete: see TODO

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -6212,7 +6212,7 @@ class Axes(_AxesBase):
                 if lbl is not None:
                     p.label = lbl
 
-                p.set_snap(False)
+                p.snap = False
 
                 for p in patch[1:]:
                     p.update(kwargs)
